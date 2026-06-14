3b3c67044a68a6ed698477868296fc8b2337491a
cndrmhrdk <cndrmhrdka@gmail.com>
2026-06-14 16:35:45 +0700

    edit dikit

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Halo! Aplikasi express ini jalan di docker!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
 });
